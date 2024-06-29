f2dcb9fbdc9974428d0107a94819e7ef73ad1747
divyasoni25 <143200499+divyasoni25@users.noreply.github.com>
2024-06-29 21:12:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for navigation links
+/*document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Dynamic year in footer
+document.getElementById('year').textContent = new Date().getFullYear();
+*/
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
